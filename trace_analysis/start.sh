79ff5c8bb709de51c077a7bce146258de4c928c0
victor <zwdong1994@gmail.com>
2019-04-17 15:39:35 +0800

    Finished the analysis code of FIU trace.

---
+++ trace_analysis/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python trace_analysis_2.py ../web_50000.trace
--- trace_analysis/trace_analysis_2.py
+++ trace_analysis/trace_analysis_2.py
@@ -1,7 +1,7 @@
 import os
 import sys
 ref = {}
-change = {}
+Max = {}
 fin_add = {}
 add_fin = {}
 
@@ -18,24 +18,22 @@ for eachline in handle:
     if add in add_fin:
         if fin in fin_add:
             if add not in fin_add[fin]:
-                change[add_fin[add]] += 1
-                ref[add_fin[add]] -= 1
+                if ref[add_fin[add]] > 0:
+                    ref[add_fin[add]] -= 1
                 fin_add[fin].append(add)
                 ref[fin] += 1
+                if ref[fin] > Max[fin]:
+                    Max[fin] = ref[fin]
                 fin_add[add_fin[add]].remove(add)
-                if ref[add_fin[add]] == 0:
-                    del fin_add[add_fin[add]]
                 add_fin[add] = fin
         else:
-            change[add_fin[add]] += 1
-            ref[add_fin[add]] -= 1
-            fin_add[add_fin[add]].remove(add)
-            if ref[add_fin[add]] == 0:
-                del fin_add[add_fin[add]]
+            if ref[add_fin[add]] > 0:
+                ref[add_fin[add]] -= 1
+                fin_add[add_fin[add]].remove(add)
             add_list = []
             add_list.append(add)
             fin_add[fin] = add_list
-            change[fin] = 0
+            Max[fin] = 1
             ref[fin] = 1
             add_fin[add] = fin
 
@@ -45,15 +43,17 @@ for eachline in handle:
             if add not in fin_add[fin]:
                 fin_add[fin].append(add)
                 ref[fin] += 1
+                if ref[fin] > Max[fin]:
+                    Max[fin] = ref[fin]
         else:
             add_list = []
             add_list.append(add)
             fin_add[fin] = add_list
-            change[fin] = 0
+            Max[fin] = 1
             ref[fin] = 1
-print "Refrence Change"
+print 'FIngerprint\t' 'Refrence\t' 'Max\t'
 for i in ref:
 #    print i
-    if change[i] > 0:
-        print ref[i],change[i]
+    if Max[i] > 0:
+        print '%s\t' % i, '%d\t' % ref[i], '%d\t' % Max[i]
 handle.close()
